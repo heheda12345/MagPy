b459d3edbf5a65726ee5a3c67b102d954eca19bd
heheda <zhangch99@outlook.com>
2023-06-06 23:40:47 +0800

    draft int tracker

---
+++ frontend/variables/base.py
@@ -0,0 +1,20 @@
+
+class Variable:
+    import functools
+    def __init__(self, expr=None, unwrap=False):
+        self.expr = expr
+        self.evaled = False
+        if unwrap:
+            self.value = self.unwrap()
+        else:
+            self.value = None
+    
+    def unwrap(self):
+        print("unwrap is called on", self)
+        if self.evaled: return self.value
+        result = self.unwrap_impl()
+        self.evaled = True
+        return result
+
+    def unwrap_impl(self):
+        raise NotImplementedError("eval_impl: should be implemented by subclasses")
