7946e684be3ef5b46feaa808b59d521e28c45583
SuperDong <16302010007@fudan.edu.cn>
2023-10-09 17:00:06 +0800

    dict done

--- frontend/variables/dict_.py
+++ frontend/variables/dict_.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Optional, Tuple, Any
+from typing import TYPE_CHECKING, Optional, Any, Callable
 from .base import Variable
 from ..fx_graph import NodeArgs, FxGraph
 from ..store_pos import StorePos, StoreInIndex
@@ -6,7 +6,7 @@ from .tensor import TensorVar
 import torch
 if TYPE_CHECKING:
     from ..pycode_generator import GraphFnCodegen, GuardFnCodegen
-    from ..object_table import ReadOnlyObjectTable, ObjectTable
+    from ..object_table import ObjectTable
 
 
 class DictVar(Variable):
@@ -17,72 +17,96 @@ class DictVar(Variable):
     def __init__(self,
                  value: dict[Any, Any],
                  need_guard_check: bool,
-                 object_table: 'ReadOnlyObjectTable',
+                 get_or_make_var: Callable[
+                     [Any, bool, Optional[FxGraph], list[StorePos]], Variable],
                  fx_graph: Optional[FxGraph] = None,
                  extract_code_at_start: list[StorePos] = []) -> None:
-        super().__init__(need_guard_check, extract_code_at_start)
+        super().__init__(need_guard_check, value, extract_code_at_start)
         self.value = value
         self.length = len(value)
         self.vars = []
         self.obj_ids = []
-        from . import make_var_from_value
-        for key, value in self.value.items():
+        for key, obj in self.value.items():
             assert not isinstance(key, torch.Tensor)
-            new_extract: list[StorePos] = [
-                StoreInIndex(pos, str(key))
-                for pos in self.extract_code_at_start
-            ]
-            var = object_table.get_or_make_var(value, need_guard_check,
-                                               fx_graph, new_extract)
+            if isinstance(key, str):
+                new_extract: list[StorePos] = [
+                    StoreInIndex(pos, id(obj), f"'{key}'")
+                    for pos in self.extract_code_at_start
+                ]
+            else:
+                new_extract = [
+                    StoreInIndex(pos, id(obj), str(key))
+                    for pos in self.extract_code_at_start
+                ]
+            var = get_or_make_var(obj, need_guard_check, fx_graph, new_extract)
             self.vars.append(var)
-            self.obj_ids.append(id(value))
+            self.obj_ids.append(id(obj))
 
     def make_guard_inner(self, codegen: "GuardFnCodegen",
                          pos: StorePos) -> None:
+        codegen.add_check(f"isinstance({pos}, dict)")
         codegen.add_check(f"len({pos}) == {self.length}")
         for key, obj in zip(self.value.keys(), self.vars):
             if not isinstance(obj, TensorVar):
-                obj.make_guard_inner(codegen, StoreInIndex(pos, str(key)))
+                if isinstance(key, str):
+                    obj.make_guard_inner(codegen,
+                                         StoreInIndex(pos, id(obj), f"'{key}'"))
+                else:
+                    obj.make_guard_inner(codegen,
+                                         StoreInIndex(pos, id(obj), str(key)))
 
-    def make_output(self, name_in_graph_fn: str, store_pos: StorePos,
-                    codegen: "GraphFnCodegen") -> None:
-        for j, var in enumerate(self.vars):
-            var.make_temp(f"{name_in_graph_fn}_{j}", store_pos, codegen)
+    def make_output_inner(self, name_in_graph_fn: str, store_pos: StorePos,
+                          codegen: "GraphFnCodegen", in_return: bool,
+                          idx: int) -> None:
+        oldest = self.get_oldest_var()
+        for j, (idx_j, var) in enumerate(zip(self.obj_ids, self.vars)):
+            var.make_output(f"{name_in_graph_fn}_{j}", store_pos, codegen,
+                            False, idx_j)
 
-        codegen.output(
-            name_in_graph_fn, store_pos,
-            f"{{{','.join(f'{key}: {name_in_graph_fn}_{j}' for key, j in zip(self.value.keys(), range(len(self.vars))))}}}"
-        )
-
-    def make_temp(self, name_in_graph_fn: str, store_pos: StorePos,
-                  codegen: "GraphFnCodegen") -> None:
-        for j, var in enumerate(self.vars):
-            var.make_temp(f"{name_in_graph_fn}_{j}", store_pos, codegen)
-        codegen.add_temp(
-            name_in_graph_fn, store_pos,
-            f"({','.join(f'{name_in_graph_fn}_{j}' for j in range(len(self.vars)))})"
-        )
+        if len(oldest.extract_code_at_start) > 0:
+            assert isinstance(oldest, DictVar)
+            old_store_pos = oldest.extract_code_at_start[0]
+            codegen.add_stmt(f"{old_store_pos}.clear()")
+            for i, key in enumerate(self.value.keys()):
+                codegen.add_stmt(
+                    f"{old_store_pos}[{key}]={name_in_graph_fn}_{i}")
+            codegen.output(name_in_graph_fn, store_pos, str(old_store_pos),
+                           in_return, idx)
+        else:
+            codegen.output(
+                name_in_graph_fn, store_pos,
+                f"{{{','.join(f'{key}: {name_in_graph_fn}_{j}' for key, j in zip(self.value.keys(), range(len(self.vars))))}}}"
+                if len(self.vars) > 0 else "{}", in_return, idx)
 
     @classmethod
     def from_value(cls,
                    value: dict[Any, Any],
                    need_guard_check: bool,
-                   object_table: 'ReadOnlyObjectTable',
+                   get_or_make_var: Callable[
+                       [Any, bool, Optional[FxGraph], list[StorePos]],
+                       Variable],
                    fx_graph: Optional[FxGraph] = None,
                    extract_code_at_start: list[StorePos] = []) -> "DictVar":
-        return cls(value, need_guard_check, object_table, fx_graph,
+        return cls(value, need_guard_check, get_or_make_var, fx_graph,
                    extract_code_at_start)
 
     def as_fx_node(self) -> NodeArgs:
         return self.value
 
     def add_subvars_to_table(self, table: 'ObjectTable') -> None:
-        for i, (var, idx) in enumerate(zip(self.vars, self.obj_ids)):
-            old_var = table.get_or_none(idx)
+        for key, var, idx in zip(self.value.keys(), self.vars, self.obj_ids):
+            old_var = table.get_or_none_by_id(idx)
             if old_var is not None:
-                new_extract: list[StorePos] = [
-                    StoreInIndex(pos, i) for pos in self.extract_code_at_start
-                ]
+                if isinstance(key, str):
+                    new_extract: list[StorePos] = [
+                        StoreInIndex(pos, idx, f"'{key}'")
+                        for pos in self.extract_code_at_start
+                    ]
+                else:
+                    new_extract = [
+                        StoreInIndex(pos, idx, str(key))
+                        for pos in self.extract_code_at_start
+                    ]
                 old_var.extract_code_at_start.extend(new_extract)
                 old_var.need_guard_check |= self.need_guard_check
             else:
--- test/test_dict.py
+++ test/test_dict.py
@@ -7,13 +7,43 @@ def without_tensor_0(a):
     return a
 
 
+def func(a, *args, b, **kwargs):
+    return a + args[0] + b + kwargs['c']
+
+
+def call_function_ex(a, *args, b, **kwargs):
+    return func(a, *args, b=b, **kwargs)
+
+
 def test_without_tensor(caplog):
     reset()
     compiled_no_tensor0 = compile(without_tensor_0)
+    compiled_no_tensor1 = compile(call_function_ex)
     a = {2.8: 1.1, 2.1: 2, 2.2: 3.3}
     result = without_tensor_0(a)
     run_and_check(compiled_no_tensor0, [MISS], 1, caplog, result, a)
     run_and_check(compiled_no_tensor0, [HIT], 1, caplog, result, a)
+    a = {}
+    result = without_tensor_0(a)
+    run_and_check(compiled_no_tensor0, [MISS], 2, caplog, result, a)
+    run_and_check(compiled_no_tensor0, [HIT], 2, caplog, result, a)
+    result = call_function_ex(1.0, 2.0, b=3.0, c=4.0)
+    run_and_check(compiled_no_tensor1, [MISS, MISS],
+                  3,
+                  caplog,
+                  result,
+                  1.0,
+                  2.0,
+                  b=3.0,
+                  c=4.0)
+    run_and_check(compiled_no_tensor1, [HIT],
+                  3,
+                  caplog,
+                  result,
+                  1.0,
+                  2.0,
+                  b=3.0,
+                  c=4.0)
 
 
 def tensor_0(a, b):
@@ -24,6 +54,7 @@ def test_with_tensor(caplog):
     reset()
     compiled_tensor0 = compile(without_tensor_0)
     compiled_tensor5 = compile(tensor_0)
+    compiled_tensor6 = compile(call_function_ex)
     a = torch.full((1,), 5.0)
     b = torch.full((1,), 7.0)
     c = torch.full((1,), 7.0)
@@ -43,3 +74,24 @@ def test_with_tensor(caplog):
     result = without_tensor_0(dict_a)
     run_and_check(compiled_tensor0, [MISS], 3, caplog, result, dict_a)
     run_and_check(compiled_tensor0, [HIT], 3, caplog, result, dict_a)
+    temp1 = torch.full((1,), 3.0)
+    temp2 = torch.full((1,), 4.0)
+    temp3 = torch.full((1,), 5.0)
+    temp4 = torch.full((1,), 6.0)
+    result = call_function_ex(temp1, temp2, b=temp3, c=temp4)
+    run_and_check(compiled_tensor6, [MISS, MISS],
+                  4,
+                  caplog,
+                  result,
+                  temp1,
+                  temp2,
+                  b=temp3,
+                  c=temp4)
+    run_and_check(compiled_tensor6, [HIT],
+                  4,
+                  caplog,
+                  result,
+                  temp1,
+                  temp2,
+                  b=temp3,
+                  c=temp4)
